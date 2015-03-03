5df57aa878322730ebf07665013156251ba2781f
rnentjes <rien@nentjes.com>
2015-03-03 23:16:02 +0100

    Working vblank interrupt

--- asm/comp_rom.sh
+++ asm/comp_rom.sh
@@ -3,7 +3,6 @@ java -jar ../lib/reasm.jar rom3.x68 MC68000
 
 cp a.out custom.rom
 length=$(stat -c%s custom.rom)
-bytes=$(expr 524272 - $length)
+bytes=$(expr 524274 - $length)
 dd if=/dev/zero of=custom.rom bs=1 count=${bytes} seek=${length}
-dd if=romend.dat of=custom.rom bs=1 count=16 seek=524272
-
+dd if=romend.dat of=custom.rom bs=1 count=14 seek=524274
